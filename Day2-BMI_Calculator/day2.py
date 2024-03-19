2fa9dc1cacc86177a71e05262ed5067ae1d45a81
ShubhamSC-23 <shubhamchavan2309@gmail.com>
2024-03-19 17:44:19 +0530

    2nd commit

---
+++ Day2-BMI_Calculator/day2.py
@@ -0,0 +1,5 @@
+weight = int(input("Enter your weight in  pounds: "))
+hight = float(input('Enter your height in meters: '))
+
+BMI = weight / (hight**2)
+print("Your BMI(Body Mass Index) is ",BMI )
